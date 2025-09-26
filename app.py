e26480af7d8bf0114e97f4d61385384cfafa94ab
D1ZERLEO <levarazorenov@mail.ru>
2025-09-26 15:12:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import requests
 
 from vk.send_message import send_deadline_message
 from school_website.about_deadlines import get_deadlines
@@ -13,16 +14,17 @@ def deadline_sender(deadlines_list):
         send_deadline_message(lesson_title)
 
 
-def add_to_the_table(deadlines_list):
+def add_to_the_table(s, deadlines_list):
     for lesson_id, lesson_title, deadline in filter_dates_in_range(
             deadlines_list, last_deadline=get_last_deadline()
     ):
-        write_lesson_homework(lesson_id, lesson_title, deadline)
+        write_lesson_homework(s, lesson_id, lesson_title, deadline)
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    deadlines = sort_by_date(get_deadlines())
-    deadline_sender(deadlines)
-    add_to_the_table(deadlines)  # если падает тут, значит, поменялся moduleid в getapihomeworks (наступил новый блок)
+    with requests.Session() as s:
+        deadlines = sort_by_date(get_deadlines(s))
+        deadline_sender(deadlines)
+        add_to_the_table(s, deadlines)
