f90fa95ac0d30d10fceb6181bcdf70a55fdb4044
D1ZERLEO <levarazorenov@mail.ru>
2025-10-17 00:18:17 +0300

    Update collect_info.py

--- google_tables/collect_info.py
+++ google_tables/collect_info.py
@@ -1,18 +1,54 @@
 from bs4 import BeautifulSoup
 from functools import reduce
-
 from google_tables.to_table import write
 from school_website.get_api_homeworks import get_homeworks
-
 import os
+
+# === Список твоей группы ===
+GROUP = [
+    "Дмитрий Постнов",
+    "Никита Морозов",
+    "Дима Бесогонов",
+    "Полина Сон",
+    "Егор Парбузин",
+    "Даниил Лучко",
+    "Тимур Махмудов",
+    "Денис Ганагин",
+    "Иван Романов",
+    "Дмитрий Нормов",
+    "Иван Шиганов",
+    "Анастасия Жихарева",
+    "Арина Конвисар",
+    "Виктория Ноубрейн",
+    "Софья Шишкина",
+    "Тимур Юлдашев",
+    "Кирилл Гнусов",
+    "Алина Колоскова",
+    "Полинка Каширская",
+    "Алексей Липский",
+    "Гаак Роман Витальевич",
+    "Зорченко Данила Сергеевич",
+    "Vlada Kalinskaya",
+    "Софа Мартынова",
+    "Степан Чугунов",
+    "Горб Вероника Александровна",
+    "Шуйская Ирина Вячеславовна",
+    "Egor Averchenkov",
+    "Айсари Светлова",
+    "Nikita Ageev",
+    "Алла Марущак",
+    "Бектагиров Даниял Тагирович",
+    "ヴォイシモイ ビラクトット",
+    "Валерия Туровская",
+    "Фрицлер Виктория Владимировна"
+]
 
 def write_lesson_homework(s, lesson_id, lesson_title, deadline):
     page = get_homeworks(s, lesson_id)
-
     soup = BeautifulSoup(page.text, "html.parser")
     print(f'Made all of the requests to the {os.getenv("API_DOMAIN")}!')
-
     print('Collect information about students...')
+
     students = []
 
     for row in soup.findAll('tr', class_='odd'):
@@ -21,21 +57,24 @@ def write_lesson_homework(s, lesson_id, lesson_title, deadline):
             continue
 
         name = items[2].get_text(strip=True)
+
+        # 🔹 Проверяем принадлежность ученика к группе
+        if name not in GROUP:
+            continue
+
         about_guy = [name]
 
-        # идём по всем домашкам, начиная с 6-го индекса
+        # 🔹 Идём по всем домашкам, начиная с 6-го индекса
         for hmw in items[6:]:
-            # проверяем, есть ли ссылка с результатами
             link = hmw.find("a", href=True)
             if link:
                 spans = [sp.get_text(strip=True) for sp in link.find_all("span")]
                 if spans:
-                    # обычно первый span = "6/6", второй = "100%"
-                    about_guy.append(spans[0])  
+                    mark = spans[0].split("/")[0]  # 🔹 Берём только число до "/"
+                    about_guy.append(mark)
                 else:
-                    about_guy.append("сдано (но без деталей)")
+                    about_guy.append("сдано")
             else:
-                # если ссылка нет, значит статусом управляет <span>
                 span = hmw.find("span")
                 if span:
                     about_guy.append(span.get_text(strip=True))
@@ -44,13 +83,11 @@ def write_lesson_homework(s, lesson_id, lesson_title, deadline):
 
         students.append(about_guy)
 
-    # сортировка студентов: сначала A-Z английские, потом остальные
+    # сортировка студентов (A-Z сначала латиница)
     students.sort(key=lambda x: (ord('A') <= ord(x[0][0].upper()) <= ord('Z'), x[0]))
 
     print('Make changes at table...')
-    from functools import reduce
     iterator = iter(reduce(lambda x, y: x + y, [k[1:] for k in students]))
-    from google_tables.to_table import write
     write(
         title=lesson_title,
         deadline=deadline,
